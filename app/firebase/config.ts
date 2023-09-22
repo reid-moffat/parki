d11dd7b073524646f31df50201578bc910f459a8
Reid Moffat <18rem8@queensu.ca>
2023-09-22 17:22:10 -0400

    firebase init

---
+++ app/firebase/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB5XlB3gS3nh5UD6ssExhXD4das2_FD_Fs",
+    authDomain: "qtma-2023-2024.firebaseapp.com",
+    projectId: "qtma-2023-2024",
+    storageBucket: "qtma-2023-2024.appspot.com",
+    messagingSenderId: "132743444471",
+    appId: "1:132743444471:web:cd8858d9e981ef6d4f60f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
