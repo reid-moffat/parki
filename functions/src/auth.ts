5a8af8e0f0a8f98fbe1b300b17f92e2bc2627c49
Reid Moffat <18rem8@queensu.ca>
2023-09-23 15:16:23 -0400

    create account function

---
+++ functions/src/auth.ts
@@ -0,0 +1,26 @@
+import {HttpsError, onCall} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import {auth} from "./helpers";
+
+const createAccount = onCall((request) => {
+        // Create user (will throw an error if the email is already in use)
+        return auth
+            .createUser({
+                email: request.data.email,
+                emailVerified: false,
+                password: request.data.password,
+                disabled: false,
+            })
+            .then(() => `Successfully created new user ${request.data.email}`)
+            .catch((error: { code: string; }) => {
+                if (error.code === 'auth/email-already-exists') {
+                    throw new HttpsError('already-exists', `Email ${request.data.email} in use`);
+                }
+
+                logger.log(`Error creating new user (not including email in use): ${JSON.stringify(error)}`);
+                throw new HttpsError('internal', `Error creating account - please try again later`);
+            });
+    }
+);
+
+export {createAccount};
