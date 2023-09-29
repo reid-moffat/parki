68db89999bf412c4edf1ac1d76d3607495b916ac
Reid Moffat <18rem8@queensu.ca>
2023-09-29 14:25:02 -0400

    doc/colelction path helper

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -1,8 +1,25 @@
 import * as admin from 'firebase-admin';
+import { HttpsError } from "firebase-functions/v2/https";
 
 // Get required services
 admin.initializeApp();
 const db = admin.firestore();
 const auth = admin.auth();
 
-export { db, auth };
+// Helpers for getting a doc/collection
+const getCollection = (path: string) => {
+    if (!/^\/[a-zA-Z0-9]+\/(([a-zA-Z0-9]+\/){2})*$/.test(path)) {
+        throw new HttpsError('internal', `Invalid collection path (${path}), see getCollection() regex`);
+    }
+
+    return db.collection(path);
+}
+const getDoc = (path: string) => {
+    if (!/^\/([a-zA-Z0-9]+\/){2}(([a-zA-Z0-9]+\/){2})*$/.test(path)) {
+        throw new HttpsError('internal', `Invalid document path (${path}), see getDoc() regex`);
+    }
+
+    return db.doc(path);
+}
+
+export { db, auth, getCollection, getDoc };
