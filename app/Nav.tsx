53290ea29255e6912721f1d3decc9df104ee0e1b
Dharsan Ravindran <dharsan.ravi07@gmail.com>
2023-10-15 17:51:30 -0400

    added pages
    routing needs fixing and updates are not reflected in this commit

---
+++ app/Nav.tsx
@@ -0,0 +1,39 @@
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import React from 'react'
+
+const Nav = () => {
+    const isUserLoggedIn = false;
+    return (
+
+
+        <nav className="flex-between w-full mb-10 p-6 ">
+            <Link href="/" className='flex gap-2 flex-center'>
+                <Image src="../assets/images/logo.svg"
+                    alt="QTMA Parking Logo"
+                    width={30}
+                    height={30}
+                    className="object-contain" />
+            </Link>
+
+            {/* Mobile Navigation */}
+
+            <div className='sm:flex hidden'>
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href="/" className="black_btn">
+                        Sign In
+                    </Link>
+
+                </div>
+
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default Nav
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Nav from './Nav'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -10,13 +11,18 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-                                       children,
-                                   }: {
+    children,
+}: {
     children: React.ReactNode
 }) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
