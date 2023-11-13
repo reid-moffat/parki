64117963632923400f2b4484fca0c4e68cbd45a6
Reid Moffat <18rem8@queensu.ca>
2023-11-13 17:03:58 -0500

    better invalid sign in details

--- app/pages/auth/signIn/page.tsx
+++ app/pages/auth/signIn/page.tsx
@@ -34,7 +34,12 @@ const SignInPage = () => {
       })
       .catch((error) => {
         console.error(`err signing in: ${error.message}`)
-        setError(error.code)
+        if (error.code === 'auth/invalid-login-credentials') {
+            // @ts-ignore
+            setError("Invalid email/password");
+        } else {
+            setError(error.code);
+        }
       })
   }
 
