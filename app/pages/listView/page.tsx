4ba51979bb6392a75ee145ba730cfea3b370a3f5
Reid Moffat <18rem8@queensu.ca>
2023-11-13 17:23:48 -0500

    unused pages

--- app/pages/listView/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const listViewPage = () => {
-  return (
-    <div>listViewPage</div>
-  )
-}
-
-export default listViewPage
--- app/pages/post/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const createPostPage = () => {
-  return (
-    <div>createPostPage</div>
-  )
-}
-
-export default createPostPage
