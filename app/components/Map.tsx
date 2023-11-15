4fc80bd56baaa32b273013cd89702f8ee3d438d6
Reid Moffat <18rem8@queensu.ca>
2023-11-14 21:56:29 -0500

    vercel error

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -4,13 +4,13 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import style from '../styles/Map.module.css'
 
-
-
 function Maps() {
-    const [center, setCenter] = useState({ lat: 44.229660, lng: -76.494186 })
+    const center = { lat: 44.229660, lng: -76.494186 };
     const ZOOM_LEVEL = 17
     const mapRef = useRef()
+
     return (
+        // @ts-ignore
         <MapContainer className={style.map} center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
