72ff4801899aeda4ce7ba69c14f5b4ea5d5452e0
Dharsan Ravindran <dharsan.ravi07@gmail.com>
2023-11-19 00:33:17 -0500

    creation of custom markers with logo

---
+++ app/components/CustomMarker.tsx
@@ -0,0 +1,26 @@
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+
+function CustomMarker(props: any) {
+    const lat = props.lat;
+    const long = props.long;
+
+
+    let DefaultIcon = L.icon({
+        iconUrl: '/PIN.png',
+        iconSize:     [38, 39],
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
+
+    return (
+        <Marker position={[lat, long]}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+    )
+}
+
+export default CustomMarker
--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,7 +3,7 @@ import React, { useRef } from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import style from '@/app/styles/Map.module.css'
-
+import CustomMarker from './CustomMarker';
 
 
 function Maps() {
@@ -17,6 +17,24 @@ function Maps() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
+
+
+            {/* This creates a custom marker at specified location points using props with latitute and longitute locations
+                We can take this further by using a database or json file and looping through to create components and
+                and display as many marker components as there are latitute and longitutde pairs in the json file/database
+            */}
+            <CustomMarker lat={44.222960} long={-76.504143}/>
+
+            <CustomMarker lat={44.225674} long={-76.498060}/>
+
+            <CustomMarker lat={44.224090} long={-76.496772}/>
+
+            <CustomMarker lat={44.226735} long={-76.499369}/>
+
+            <CustomMarker lat={44.226739} long={-76.501799}/>
+
+
+
         </MapContainer>
     )
 }
