2c7fc3da17bfdae584a66d71c40f6cc3b18a44e0
Reid Moffat <18rem8@queensu.ca>
2023-11-20 22:05:28 -0500

    display dummy data

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -4,11 +4,12 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import style from '@/app/styles/Map.module.css'
 import CustomMarker from './CustomMarker';
+import dummyData from "@/app/pages/map/dummyData";
 
 
 function Maps() {
-    const center = { lat: 44.226795, lng: -76.498151 };
-    const ZOOM_LEVEL = 16;
+    const center = { lat: 44.226795, lng: -76.495151 };
+    const ZOOM_LEVEL = 14.5;
     const mapRef = useRef();
 
     return (
@@ -28,15 +29,10 @@ function Maps() {
                 We can take this further by using a database or json file and looping through to create components and
                 and display as many marker components as there are latitute and longitutde pairs in the json file/database
                 */}
-                <CustomMarker lat={44.222960} long={-76.504143}/>
 
-                <CustomMarker lat={44.225674} long={-76.498060}/>
-
-                <CustomMarker lat={44.224090} long={-76.496772}/>
-
-                <CustomMarker lat={44.226735} long={-76.499369}/>
-
-                <CustomMarker lat={44.226739} long={-76.501799}/>
+                {
+                    dummyData.map((data) => <CustomMarker lat={data.latitude} long={data.longitude}/>)
+                }
 
             </MapContainer>
         </>
