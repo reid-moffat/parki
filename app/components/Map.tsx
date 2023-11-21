414ac6d39ebeb6c9cb89e61d335440c9d955be23
Reid Moffat <18rem8@queensu.ca>
2023-11-20 21:23:58 -0500

    added header

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -12,30 +12,34 @@ function Maps() {
     const mapRef = useRef();
 
     return (
-        <MapContainer className={style.map} center={center} zoom={ZOOM_LEVEL}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
+        <>
+            <div className={style.header}>
+                <text className={style.headerText}>PARKI</text>
+            </div>
+
+            <MapContainer className={style.map} center={center} zoom={ZOOM_LEVEL}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
 
 
-            {/* This creates a custom marker at specified location points using props with latitute and longitute locations
+                {/* This creates a custom marker at specified location points using props with latitute and longitute locations
                 We can take this further by using a database or json file and looping through to create components and
                 and display as many marker components as there are latitute and longitutde pairs in the json file/database
-            */}
-            <CustomMarker lat={44.222960} long={-76.504143}/>
+                */}
+                <CustomMarker lat={44.222960} long={-76.504143}/>
 
-            <CustomMarker lat={44.225674} long={-76.498060}/>
+                <CustomMarker lat={44.225674} long={-76.498060}/>
 
-            <CustomMarker lat={44.224090} long={-76.496772}/>
+                <CustomMarker lat={44.224090} long={-76.496772}/>
 
-            <CustomMarker lat={44.226735} long={-76.499369}/>
+                <CustomMarker lat={44.226735} long={-76.499369}/>
 
-            <CustomMarker lat={44.226739} long={-76.501799}/>
+                <CustomMarker lat={44.226739} long={-76.501799}/>
 
-
-
-        </MapContainer>
+            </MapContainer>
+        </>
     )
 }
 
