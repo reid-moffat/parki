cd849edee393514778e9799d5a07e7db377ac707
Reid Moffat <18rem8@queensu.ca>
2023-11-28 19:24:58 -0500

    vercel fix (hopefully)

--- app/pages/map/page.tsx
+++ app/pages/map/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import style from '@/app/styles/Map.module.css'
 import Image from 'next/image';
 import logo from "@/public/logo.png";
@@ -9,47 +9,14 @@ import profileIcon from "@/public/map/user.png";
 import mapIcon from "@/public/map/map.png";
 import settingsIcon from "@/public/map/settings.png";
 
-// Gets window dimensions (and updates if they change)
-const useWindowSize = () => {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowSize({
-                // @ts-ignore
-                width: window.innerWidth,
-                // @ts-ignore
-                height: window.innerHeight,
-            });
-        }
-
-        window.addEventListener("resize", handleResize); // Update on window resize
-
-        // Call right away to get initial dimensions, and clean up event listener
-        handleResize();
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    return windowSize;
-}
-
 const MapSelectionPage = () => {
-    let size = { width: undefined, height: undefined};
-    if (typeof window !== "undefined") {
-        size = useWindowSize();
-    }
-
     return (
         <div style={{'backgroundColor': '#343632'}}>
-            <div className={style.logoContainer}>
-                {/* @ts-ignore */}
-                <Image className={style.logo} src={logo} width={size.width * 0.25} height={size.height * 0.1}
-                       alt="Parki logo"/>
+            <div className={style.headerContainer}>
+                <div className={style.logoContainer}>
+                    {/* @ts-ignore */}
+                    <Image src={logo} alt="Parki logo"/>
+                </div>
             </div>
 
             <div className={style.mapContainer}>
