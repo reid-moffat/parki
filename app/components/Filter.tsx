6306e72f13328be5072eae5dbae373111733b53e
Reid Moffat <18rem8@queensu.ca>
2023-11-30 00:42:48 -0500

    basic filter page

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import { MdArrowBackIos } from "react-icons/md";
 import { FaFilterCircleXmark } from "react-icons/fa6";
+import { LuClock5 } from "react-icons/lu";
 import { States } from "@/app/pages/map/page";
+import Divider from "@/public/Line.png";
+import Image from 'next/image';
+import { FaWheelchair } from "react-icons/fa";
+import { IconContext } from 'react-icons';
+import { MdLocalParking } from "react-icons/md";
+import { RiBattery2ChargeLine } from "react-icons/ri";
+import { FaCarTunnel } from "react-icons/fa6";
+import { IoSnowSharp } from "react-icons/io5";
 
 // @ts-ignore
 const FilterPage = ({ setPageState }) => {
+    const renderAmenities = () => {
+        const amenities = [
+            [<FaWheelchair/>, "Accessible"],
+            [<MdLocalParking/>, "Self-Park"],
+            [<RiBattery2ChargeLine/>, "EV Charging"],
+            [<FaCarTunnel/>, "Covered"],
+            [<FaWheelchair/>, "On-Site Staff"],
+            [<IoSnowSharp/>, "Shovelling Included"],
+        ];
+
+        return amenities.map((amenity) => (
+            <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center w-3/6 rounded-full text-white bg-[#343632]">
+                    <IconContext.Provider value={{ color: '#FF4251' }}>
+                        {amenity[0]}
+                    </IconContext.Provider>
+                    &nbsp;
+                    {amenity[1]}
+                </div>
+                <br/><br/>
+            </div>
+        ));
+    }
+
     return (
         <div className="absolute ml-[2vw] h-[86.5vh] w-[96vw] rounded-xl z-50 bg-[#FCF9EF] text-[#343632]">
 
@@ -17,9 +50,34 @@ const FilterPage = ({ setPageState }) => {
                 <MdArrowBackIos color="transparent"/>
             </div>
 
-            <div>
-                <div className="text-center rotate-12 text-xl font-bold mt-32 text-[#FF4251]">filters coming soon!</div>
+            <div className="text-center font-bold">
+                Parking Ending On
             </div>
+            <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center w-3/6 rounded-full text-white bg-[#343632]">
+                    <LuClock5/> &nbsp; December 20, 2023
+                </div>
+            </div>
+            <br/>
+            <div className="text-center">
+                Within <span className="text-blue-500">25</span> km
+            </div>
+            [Distance slider]
+
+            <div className="text-center">
+                Price Range:
+                <p/>
+                $<span className="text-blue-500">0</span> to $<span className="text-blue-500">5</span>
+            </div>
+            [Price slider]
+            <br/><br/><br/>
+
+            <div className="flex justify-center items-center">
+                <Image src={Divider} alt={"Divider"}/>
+            </div>
+            <br/><br/>
+
+            {renderAmenities()}
         </div>
     );
 }
