072e7b96346c5a634cffd2fd37f6d4a9328484b3
Reid Moffat <18rem8@queensu.ca>
2024-01-23 00:16:13 -0500

    misc

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,7 @@ import React from "react";
 
 const inter = Inter({subsets: ['latin']})
 
-export default function RootLayout({ children }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body className={inter.className}>{children}</body>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async redirects() {
-        // The root page automatically routes to the profile screen
+        // The root page automatically routes to the map screen
         return [
             {
                 source: '/',
