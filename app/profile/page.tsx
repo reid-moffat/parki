6ad25cc127f17129c2350aeee2082a7aba0fb64b
Reid Moffat <18rem8@queensu.ca>
2024-01-23 23:29:33 -0500

    simplify auth structure

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Profile = () => {
     return (
         <div>
-            Profile page (once user is signed in)
+            If the user is not signed n:
+            -This is the top left and bottom left in figma auth (create acc/sign in buttons, and sign in w/ google/email)
+            If the user is signed in:
+            -This is the profile page
         </div>
     );
 }
