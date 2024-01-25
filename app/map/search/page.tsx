31f95c24834835ef0f1cd1f80438790b358a21fa
Reid Moffat <18rem8@queensu.ca>
2024-01-24 21:57:53 -0500

    search bottom bar

--- app/map/search/page.tsx
+++ app/map/search/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import BottomBar from "@/components/helpers/BottomBar";
 
 const Search = () => {
     return (
-        <div>
+        <div style={{ backgroundColor: '#343632', position: 'absolute', height: '100vh', width: '100vw', zIndex: '10' }}>
             Page for searching addresses
+            <BottomBar/>
         </div>
     );
 }
