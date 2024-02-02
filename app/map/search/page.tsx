e6f27b8942eab3c2cb4a4bb2fc34f4ab865796c4
Reid Moffat <18rem8@queensu.ca>
2024-02-01 21:53:29 -0500

    lookin good

--- app/map/search/page.tsx
+++ app/map/search/page.tsx
@@ -31,10 +31,10 @@ const Search = () => {
             <Image src={logo} alt="Parki logo" className='w-[120vw] h-[8vh] object-contain mt-3 mb-4'/>
 
             <div className="ml-[2vw] h-[79.5vh] w-[96vw] pt-[2vh] rounded-xl bg-[#FCF9EF] font-mono">
-                <div className="ml-[4vw] h-[6vh] w-[88vw] rounded-[2rem] border-black border-solid border-[1px]">
-                    <Image src={Back} alt={"Go back"} className={"w-[2vw] float-left"}/>
-                    <input type="text" className={"bg-black pl-[20vw]"}/>
-                    <Image src={X} alt={"Clear search query"} className={"w-[2vw] float-right"}/>
+                <div className="ml-[4vw] h-[6vh] w-[88vw] rounded-[2rem] border-black border-solid border-[1px] inline-flex">
+                    <Image src={Back} alt={"Go back"} className={"w-[3vw] h-[3vh] ml-[6vw] mt-[1.5vh]"}/>
+                    <input type="text" className={"w-[55vw] ml-[8vw] mt-[1vh] mb-[1vh] bg-[#FCF9EF]"} placeholder={"Search..."}/>
+                    <Image src={X} alt={"Clear search query"} className={"w-[4vw] h-[4vw] mt-[2vh] ml-[7vw]"}/>
                 </div>
 
                 {adresses.map((address, index) =>
@@ -45,12 +45,12 @@ const Search = () => {
                                 alt={"Clock icon"}
                                 className={"w-[8vw] h-[9vw] mt-[2vh] ml-[8vw] mb-[2vh]"}
                             />
-                            <div className={"w-[50vw] ml-[10vw] mt-[1vh]"}>
+                            <div className={"w-[40vw] ml-[10vw] mt-[1vh]"}>
                                 {address.street}
                                 <br/>
                                 {address.city}
                             </div>
-                            <Image src={Arrow} alt={"Arrow icon"} className={"w-[7vw] h-[7vw] mt-[2vh] ml-auto"}/>
+                            <Image src={Arrow} alt={"Arrow icon"} className={"w-[7vw] h-[7vw] mt-[2vh] ml-[16vw]"}/>
                         </div>
                         <Image src={Line} alt={"Line icon"} className={"w-[80vw] ml-[8vw]"}/>
                     </>
