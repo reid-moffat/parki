2685ea51f3454f084604d114470225f6752cc64e
Reid Moffat <18rem8@queensu.ca>
2024-02-01 19:47:34 -0500

    page formet

--- app/map/search/page.tsx
+++ app/map/search/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import BottomBar from "@/components/helpers/BottomBar";
+import logo from "@/public/logo.png";
+import Image from "next/image";
 
 const Search = () => {
     return (
         <div style={{ backgroundColor: '#343632', position: 'absolute', height: '100vh', width: '100vw', zIndex: '10' }}>
-            Page for searching addresses
+            <Image src={logo} alt="Parki logo" className='w-[120vw] h-[8vh] object-contain mt-3 mb-4'/>
+
+            <div className="absolute ml-[2vw] h-[79.5vh] w-[96vw] rounded-xl -z-50 bg-[#FCF9EF]">
+                test
+            </div>
             <BottomBar/>
         </div>
     );
