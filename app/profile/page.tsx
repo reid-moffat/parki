bb9d9ac0da0999f0ab73202950361a59d09106d4
connorrewa <connorrewa13@hotmail.com>
2024-02-01 21:07:31 -0500

    changed create account button on profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -19,7 +19,12 @@ const Profile = () => {
                 <Image src={parkilogo} alt="" className='parki-logo'/>
             </div>
             <div className='button-container'>
-                <button className='create-account'>Create Account</button>
+                <Link 
+                    className='create-account'
+                    href={{ pathname: '/profile/signUp'}}
+                >
+                    Create Account
+                </Link>
                 <Link
                     className='sign-in'/*USED MANUAL CSS, THIS TALIWIND IS BETTER --> className="flex flex-row items-center bg-transparent w-4/5 py-2 rounded-lg mt-3 mx-auto text-[#FCF9EF] border-2 border-[#FCF9EF]"*/ 
                     href={{ pathname: '/profile/signIn' }}
