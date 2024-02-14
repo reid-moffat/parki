2b4db40514917e4de72ab39fc309c8666e2dbc88
Reid Moffat <18rem8@queensu.ca>
2024-02-13 23:02:52 -0500

    css -> tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body className={inter.className}>
-        <div style={{backgroundColor: '#343632', position: 'absolute', height: '100vh', width: '100vw', zIndex: '10'}}>
+        <div className="absolute h-full w-full bg-[#343632]">
             <Image src={logo} alt="Parki logo" className='w-[100vw] h-[8vh] object-contain mt-3 mb-4'/>
             <div className="ml-[2vw] h-[79.5vh] w-[96vw] rounded-xl  bg-[#FCF9EF] text-[#343632] -z-50">
                 {children}
