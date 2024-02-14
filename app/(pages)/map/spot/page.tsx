606c1325feb340f51c2ea67cb8a487cb1dac45e0
Reid Moffat <18rem8@queensu.ca>
2024-02-14 14:30:47 -0500

    detail bugfix

--- app/(pages)/map/spot/page.tsx
+++ app/(pages)/map/spot/page.tsx
@@ -11,8 +11,12 @@ import { IoSnowSharp } from "react-icons/io5";
 import { IconContext } from "react-icons";
 import Divider from "@/components/helpers/Divider";
 import Link from "next/link";
+import { useSelector } from "react-redux";
+import { getSpot } from "@/app/GlobalRedux/Features/currentSpot";
 
 const DetailsPage = () => {
+
+    const currentSpot = useSelector(getSpot);
 
     const renderAmenities = () => {
         const allAmenities = [
@@ -24,7 +28,8 @@ const DetailsPage = () => {
             <IoSnowSharp key={"Shovelling Included"}/>,
         ];
 
-        return allAmenities.filter((amenity) => searchParams.amenities?.includes(amenity.key))
+        // @ts-ignore
+        return allAmenities.filter((amenity) => currentSpot.amenities?.includes(amenity.key))
             .map((amenity) => {
                 return (
                     <div className="flex justify-center items-center" key={amenity.key}>
@@ -55,13 +60,15 @@ const DetailsPage = () => {
                 <Image src={car} alt={"Parking_Lot_pic"} className='border-0 rounded-xl overflow-hidden'/>
 
                 <div className='p-5'>
-                    <div className='text-[1.75rem] font-normal'>{searchParams.address}</div>
+                    { /* @ts-ignore */ }
+                    <div className='text-[1.75rem] font-normal'>{currentSpot.address}</div>
                     <div className='flex flex-row w-[100%] items-center -mt-3'>
                         <Image src={locationIcon} alt={"Location_Icon"} className='w-[5%] h-[5%] mr-3'/>
                         <div className='text-[1.25rem]'>Kingston, ON</div>
                     </div>
                     <div className='text-[1rem] font-outfit mt-3'>
-                        {searchParams.description}
+                        { /* @ts-ignore */ }
+                        {currentSpot.description}
                     </div>
                 </div>
 
@@ -79,7 +86,8 @@ const DetailsPage = () => {
                     <div className='flex flex-row justify-between items-center'>
                         <div className='text-[1.5rem] font-normal'>Reviews</div>
                         <div className="flex items-center">
-                            <div className='text-[1.125rem] font-bold mr-1'>{searchParams.rating}</div>
+                            { /* @ts-ignore */ }
+                            <div className='text-[1.125rem] font-bold mr-1'>{currentSpot.rating}</div>
                             stars
                         </div>
                     </div>
@@ -94,7 +102,8 @@ const DetailsPage = () => {
             <div
                 className='absolute w-[96vw] pl-[5%] pr-[5%] bottom-[20vw] flex flex-row h-[8%] bg-[#ff4251] rounded-b-xl justify-between items-center'
             >
-                <div className='text-[#FCF9EF] text-[1.75rem] font-normal'>${searchParams.price}/month</div>
+                { /* @ts-ignore */ }
+                <div className='text-[#FCF9EF] text-[1.75rem] font-normal'>${currentSpot.price}/month</div>
                 <Link href='/map/spot/policy'
                       className='text-white text-xl bg-[#343632] p-2 rounded-xl ps-8 pe-8'>RESERVE</Link>
             </div>
