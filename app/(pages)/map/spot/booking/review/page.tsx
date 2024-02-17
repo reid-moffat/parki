b05a7fe4624e34e9e7ae8232ff41806418fa39f3
Reid Moffat <18rem8@queensu.ca>
2024-02-16 23:54:14 -0500

    review summary

--- app/(pages)/map/spot/booking/review/page.tsx
+++ app/(pages)/map/spot/booking/review/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { MdOutlineArrowBackIos } from "react-icons/md";
+import React from "react";
+
 const ReviewBooking = () => {
     return (
         <div>
-            ReviewBooking
+            <div className='flex text-3xl font-bold p-8'>
+                <Link href="/map/spot/booking">
+                    <MdOutlineArrowBackIos className='w-10 h-10 mr-2'/>
+                </Link>
+                <div className="pl-2">
+                    Review Summary
+                </div>
+            </div>
         </div>
     );
 }
