d648d1c063a1563e082458291e800c23ef8a2760
Reid Moffat <18rem8@queensu.ca>
2024-02-17 00:08:39 -0500

    review page

--- app/(pages)/map/spot/booking/review/page.tsx
+++ app/(pages)/map/spot/booking/review/page.tsx
@@ -13,6 +13,85 @@ const ReviewBooking = () => {
                     Review Summary
                 </div>
             </div>
+
+            <div className="rounded-xl border-2 border-black mt-4 ml-10 mr-10 pt-4 pb-4 pl-4 pr-4 font-outfit">
+                <div className="flex justify-between">
+                    <div>
+                        Address
+                    </div>
+                    <div className="font-bold">
+                        163 Union Street
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div>
+                        Vehicle Make
+                    </div>
+                    <div className="font-bold">
+                        Honda Civic
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div>
+                        License
+                    </div>
+                    <div className="font-bold">
+                        HYAN 041
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div>
+                        Date
+                    </div>
+                    <div className="font-bold">
+                        December 12-15
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div>
+                        Duration
+                    </div>
+                    <div className="font-bold">
+                        4 days
+                    </div>
+                </div>
+            </div>
+
+            <div className="rounded-xl border-2 border-black mt-4 ml-10 mr-10 pt-4 pb-4 pl-4 pr-4 font-outfit">
+                <div className="flex justify-between">
+                    <div>
+                        Amount
+                    </div>
+                    <div className="font-bold">
+                        $36.00
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div>
+                        Taxes (13% HST)
+                    </div>
+                    <div className="font-bold">
+                        $4.68
+                    </div>
+                </div>
+                <div className="bg-black h-[0.5px] mt-2 mb-2"/>
+                <div className="flex justify-between">
+                    <div>
+                        Total
+                    </div>
+                    <div className="font-bold">
+                        $40.68
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-center mt-8">
+                <div
+                    className="rounded-2xl bg-[#FF4251] p-2 text-center text-white text-xl pl-14 pr-14 mt-10"
+                >
+                    CONFIRM PAYMENT
+                </div>
+            </div>
         </div>
     );
 }
