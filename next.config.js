01835422ca9dfc786eefb9b978d29c634b63897d
Reid Moffat <18rem8@queensu.ca>
2024-02-27 16:29:51 -0500

    redirect to profile

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/map',
+                destination: '/profile',
                 permanent: false,
             }
         ];
