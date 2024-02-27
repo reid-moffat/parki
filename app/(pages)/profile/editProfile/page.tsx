e35af2f41ff56c3a6a6db0738d0ac58d4666eb51
Reid Moffat <18rem8@queensu.ca>
2024-02-27 18:28:57 -0500

    phone #

--- app/(pages)/profile/editProfile/page.tsx
+++ app/(pages)/profile/editProfile/page.tsx
@@ -24,13 +24,47 @@ const EditProfile = () => {
                 console.log(`Profile info: ${JSON.stringify(response.data, null, 4)}`);
 
                 // @ts-ignore
-                setName(response.data.name); // @ts-ignore
-                setEmail(response.data.email); // @ts-ignore
-                setPhone(response.data.phone); // @ts-ignore
-                setEtransfer(response.data.etransfer);
+                setName(response.data.name ?? ""); // @ts-ignore
+                setEmail(response.data.email ?? ""); // @ts-ignore
+                setPhone(response.data.phone ?? ""); // @ts-ignore
+                setEtransfer(response.data.etransfer ?? "");
             })
             .catch((err) => console.log(`Error fetching profile info: ${err}`));
     });
+
+    const updatePhone = (number: string) => {
+        number = number.replace(/[^0-9]/g, '');
+        if ((number.substring(number.length - 1) < '0' || number.substring(number.length - 1) > '9') && number.length > 0) {
+            return;
+        }
+        if (number.length > 10) {
+            return;
+        }
+        setPhone(number);
+    }
+
+    const renderPhone = (number: string) => {
+        const numberCopy = number.slice();
+        if (numberCopy.length <= 3) {
+            return numberCopy;
+        } else if (numberCopy.length <= 6) {
+            return `(${numberCopy.substring(0, 3)})-${numberCopy.substring(3, 6)}`;
+        } else {
+            return `(${numberCopy.substring(0, 3)})-${numberCopy.substring(3, 6)}-${numberCopy.substring(6, 10)}`;
+        }
+    }
+
+    const handleUpdate = async () => {
+        const data = {
+            name: name,
+            phone: phone,
+            etransfer: etransfer
+        };
+
+        httpsCallable(functions, "updateProfile")(data)
+            .then((response) => console.log(`Profile updated: ${response.data}`))
+            .catch((err) => console.log(`Error updating profile: ${err}`));
+    }
 
     return (
         <div className="mx-10">
@@ -57,17 +91,16 @@ const EditProfile = () => {
             />
 
             <div>Email</div>
-            <div
+            <input
                 className='w-full py-3 px-5 mb-1 outline-none rounded-2xl bg-[#4472CA] bg-opacity-20'
-            >
-                {email}
-            </div>
+                value={email}
+            />
 
             <div>Phone number</div>
             <input
                 className='w-full py-3 px-5 mb-1 outline-none rounded-2xl bg-[#4472CA] bg-opacity-20'
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
+                value={renderPhone(phone)}
+                onChange={(e) => updatePhone(e.target.value)}
             />
 
             <div>E-Transfer address</div>
@@ -81,7 +114,10 @@ const EditProfile = () => {
                 <div className="underline font-bold">
                     Cancel
                 </div>
-                <div className="text-center text-white text-lg font-bold rounded-xl bg-[#FF4251] py-1 px-12">
+                <div
+                    className="text-center text-white text-lg font-bold rounded-xl bg-[#FF4251] py-1 px-12"
+                    onClick={async () => await handleUpdate()}
+                >
                     Update
                 </div>
             </div>
