06d5495ab81edd6a55ee822df023a06b0526709a
Dharsan Ravindran <dharsan.ravi07@gmail.com>
2024-02-28 22:01:08 -0500

    back button

--- app/(pages)/map/search/page.tsx
+++ app/(pages)/map/search/page.tsx
@@ -12,6 +12,7 @@ import { setLocation, getLocation } from "@/app/GlobalRedux/Features/search";
 import { fromAddress, OutputFormat, setDefaults } from "react-geocode";
 import { useRouter } from "next/navigation";
 import { callApi } from "@/config/firebase";
+import { MdChevronLeft } from 'react-icons/md';
 
 const Search = () => {
 
@@ -88,7 +89,9 @@ const Search = () => {
             <div
                 className="ml-[4vw] mt-6 h-[6vh] w-[88vw] rounded-[2rem] border-black border-solid border-[1px] inline-flex font-mono">
                 <Link href="/map">
-                    <Image src={Back} alt={"Go back"} className={"w-[3vw] h-[3vh] ml-[6vw] mt-[1.5vh]"}/>
+                    <div className="pt-1">
+                        <MdChevronLeft size={42}/>
+                    </div>
                 </Link>
                 <input
                     type="text"
@@ -119,7 +122,7 @@ const Search = () => {
                                 />
                                 <div className={"w-[50vw] ml-[10vw] mt-[1vh]"}>
                                     {address.street}
-                                    <br/>
+                                    <br />
                                     {address.city}
                                 </div>
                                 <Link href="/map">
@@ -130,7 +133,7 @@ const Search = () => {
                                     />
                                 </Link>
                             </div>
-                            <Image src={Line} alt={"Line icon"} className={"w-[80vw] ml-[8vw]"}/>
+                            <Image src={Line} alt={"Line icon"} className={"w-[80vw] ml-[8vw]"} />
                         </>
                     )}
             </div>
--- app/(pages)/spots/addSpot/page.tsx
+++ app/(pages)/spots/addSpot/page.tsx
@@ -142,56 +142,56 @@ const AddSpot = () => {
             </>
         ),
         4: (
-            <div className="border border-gray-400 p-4 rounded-xl">
-                <div className="text-3xl font-bold">
-                    Upload photos
-                </div>
+<div className="border border-gray-400 p-4 rounded-xl">
+            <div className="text-3xl font-bold">
+                Upload photos
+            </div>
 
-                <div className="mt-4">
-                    Hold and drag to rearrange photos!
-                </div>
+            <div className="mt-4">
+                Hold and drag to rearrange photos!
+            </div>
 
-                <div className="text-gray-600">
-                    <u>Photo Tips:</u> Take the photo during the day with plenty of natural light! Make sure to include
-                    any elements that will help drivers identify the specific parking spot.
-                </div>
+            <div className="text-gray-600">
+                <u>Photo Tips:</u> Take the photo during the day with plenty of natural light! Make sure to include
+                any elements that will help drivers identify the specific parking spot.
+            </div>
 
-                <div className="border border-gray-400 p-4 rounded-xl mt-6" onClick={handleDivClick}>
-                    <div className="flex justify-center items-center h-32 border-dashed border-4 border-gray-400 rounded-xl">
-                        {selectedFiles ? (
-                            Array.from(selectedFiles).map((file, index) => (
-                                <div key={index} className="relative w-24 h-24 mr-4">
-                                    <img
-                                        src={URL.createObjectURL(file)}
-                                        alt={`Uploaded ${index + 1}`}
-                                        className="w-full h-full object-cover rounded-md border border-gray-400"
-                                    />
-                                </div>
-                            ))
-                        ) : (
-                            <div className="text-center">
-                                <FaPlus className="text-5xl mb-1" />
-                                Add Photo
+            <div className="border border-gray-400 p-4 rounded-xl mt-6" onClick={handleDivClick}>
+                <div className="flex justify-center items-center h-32 border-dashed border-4 border-gray-400 rounded-xl">
+                    {selectedFiles ? (
+                        Array.from(selectedFiles).map((file, index) => (
+                            <div key={index} className="relative w-24 h-24 mr-4">
+                                <img
+                                    src={URL.createObjectURL(file)}
+                                    alt={`Uploaded ${index + 1}`}
+                                    className="w-full h-full object-cover rounded-md border border-gray-400"
+                                />
                             </div>
-                        )}
-                    </div>
-                </div>
-
-                <input
-                    ref={fileInputRef}
-                    type="file"
-                    style={{ display: "none" }}
-                    onChange={handleFileChange}
-                    multiple
-                />
-
-                <div
-                    className="mt-6 bg-red-500 text-white text-center py-2 rounded-xl cursor-pointer"
-                    onClick={handleConfirmPhotos}
-                >
-                    Confirm Photos
+                        ))
+                    ) : (
+                        <div className="text-center">
+                            <FaPlus className="text-5xl mb-1" />
+                            Add Photo
+                        </div>
+                    )}
                 </div>
             </div>
+
+            <input
+                ref={fileInputRef}
+                type="file"
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+                multiple
+            />
+
+            <div
+                className="mt-6 bg-red-500 text-white text-center py-2 rounded-xl cursor-pointer"
+                onClick={handleConfirmPhotos}
+            >
+                Confirm Photos
+            </div>
+        </div>
         ),
         5: (
             <>
@@ -233,6 +233,7 @@ const AddSpot = () => {
                 <MdChevronLeft size={42} onClick={onClickBack} />
             </div>
 
+            
             { /* @ts-ignore */}
             {steps[currentStep]}
         </div>
